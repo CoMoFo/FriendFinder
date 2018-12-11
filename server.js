ef78ef184994e99934af398473dc0526d235d41c
CoMoFo <comofodo@gmail.com>
2018-12-10 22:21:00 -0500

    Minor tweaks, prettyfying

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var PORT = process.env.PORT || 8081;
+var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("app/public"));
